56cfe55742c6fbaf3ad6d62dd4021ec318ae52ac
Ali Syed <alisyed0206@gmail.com>
2024-09-10 11:12:00 -0400

    add starter code to repo

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Ali Syed on 2024-09-10.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
